47b936f746be157844ad0a11d89cb8a0f8d87635
yonis <yoonis7881@gmail.com>
2023-12-18 10:43:34 +0300

    Built Search Input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,7 +5,7 @@ const ColorModeSwitch = () => {
   return (
 <HStack >
     <Switch colorScheme="green" isChecked={colorMode === "dark"}  onChange={toggleColorMode}/>
-    <Text>
+    <Text whiteSpace={"nowrap"} marginRight={3}>
         Dark Mode
     </Text>
 </HStack>
