1e16f16832106215d659959f6beb9a3766ae7f9b
yonis <yoonis7881@gmail.com>
2023-12-24 10:49:15 +0300

    Changing Game Title

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,26 +3,24 @@ import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
 
+interface Props {
+  onSearch: (searchText: string) => void;
+}
 
-
-export const SearchInput = ({onSearch}: Props) => {
+const SearchInput = ({ onSearch }: Props) => {
   const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if(ref.current)  onSearch(ref.current.value)
-      }}
-    >
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      if (ref.current) onSearch(ref.current.value);
+    }}>
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
-        <Input
-          ref={ref}
-          borderRadius={20}
-          placeholder="Search games..."
-          variant="filled"
-        />
+        <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="filled" />
       </InputGroup>
     </form>
   );
 };
+
+export default SearchInput;
